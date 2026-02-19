b4a684f42062ebb959144535e07619412d4f2c28
Boris Lopes <boris@yolobank.com>
2026-02-19 10:11:37 -0100

    WIP

--- lua/plugins/mason-lsconfig.lua
+++ lua/plugins/mason-lsconfig.lua
@@ -1,26 +1,47 @@
 return {
   'neovim/nvim-lspconfig',
-  version = 'v2.*',
   dependencies = {
     { 'williamboman/mason.nvim', config = true },
-    { 'williamboman/mason-lspconfig.nvim', version = 'v1.*' },
+    { 'williamboman/mason-lspconfig.nvim' },
     { 'j-hui/fidget.nvim', opts = {} },
   },
   config = function()
-    local lspconfig = require('lspconfig')
     local mason_lspconfig = require('mason-lspconfig')
 
     mason_lspconfig.setup({
-      ensure_installed = { 'terraformls', 'tflint' },
+      -- Added: rust_analyzer
+      ensure_installed = { 
+        'terraformls', 
+        'tflint', 
+        'ts_ls', 
+        'html', 
+        'cssls', 
+        'lua_ls',
+        'rust_analyzer'
+      },
     })
 
+    -- The New 0.11+ Handler
     mason_lspconfig.setup_handlers({
       function(server_name)
-        lspconfig[server_name].setup({})
+        -- Special config for Lua
+        if server_name == "lua_ls" then
+          vim.lsp.config("lua_ls", {
+            settings = {
+              Lua = {
+                diagnostics = { globals = { "vim" } },
+                workspace = { checkThirdParty = false },
+              },
+            },
+          })
+        end
+
+        -- Note: rust_analyzer works great with default settings in 0.11
+        vim.lsp.enable(server_name)
       end,
     })
 
-    -- Filetype detection fix
+    -- Filetype detection fix for Terraform/HCL
     vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
       pattern = {"*.tf", "*.tfvars", "*.hcl"},
       callback = function()
@@ -28,12 +49,15 @@ return {
       end,
     })
 
-    -- Format on save
+    -- Format on save (Added *.rs)
     vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = {"*.tf", "*.tfvars"},
+      pattern = {
+        "*.tf", "*.tfvars", "*.js", "*.ts", 
+        "*.html", "*.css", "*.lua", "*.rs"
+      },
       callback = function()
         vim.lsp.buf.format()
       end,
     })
-  end, -- This closes the 'config' function
+  end,
 }
