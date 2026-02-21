9e1965704a2ec161f77f1eddf182bcc7c70ff774
Boris Lopes <boris@yolobank.com>
2026-02-21 13:53:02 -0100

    Add Multi Line

--- lua/plugins/mason-lsconfig.lua
+++ lua/plugins/mason-lsconfig.lua
@@ -1,63 +1,90 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    { 'williamboman/mason.nvim', config = true },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'j-hui/fidget.nvim', opts = {} },
-  },
-  config = function()
-    local mason_lspconfig = require('mason-lspconfig')
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		{ 'williamboman/mason.nvim',          config = true },
+		{ 'williamboman/mason-lspconfig.nvim' },
+		{ 'j-hui/fidget.nvim',                opts = {} },
+	},
+	config = function()
+		-- 1. Global Diagnostic Settings (This makes errors/warnings visible)
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = '●', -- Displays the message at the end of the line
+				spacing = 4,
+			},
+			signs = true, -- Show icons in the gutter
+			underline = true, -- Underline error text
+			update_in_insert = false,
+			severity_sort = true,
+			float = {
+				border = 'rounded',
+				source = 'always', -- Shows which LSP provided the error
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- Added: rust_analyzer
-      ensure_installed = { 
-        'terraformls', 
-        'tflint', 
-        'ts_ls', 
-        'html', 
-        'cssls', 
-        'lua_ls',
-        'rust_analyzer'
-      },
-    })
+		-- 2. Set Gutter Icons (Optional but highly recommended)
+		local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "»" }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
 
-    -- The New 0.11+ Handler
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        -- Special config for Lua
-        if server_name == "lua_ls" then
-          vim.lsp.config("lua_ls", {
-            settings = {
-              Lua = {
-                diagnostics = { globals = { "vim" } },
-                workspace = { checkThirdParty = false },
-              },
-            },
-          })
-        end
+		-- 3. Diagnostic Keymaps (To navigate and read errors)
+		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
+		vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error message' })
+		vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic list' })
 
-        -- Note: rust_analyzer works great with default settings in 0.11
-        vim.lsp.enable(server_name)
-      end,
-    })
+		local mason_lspconfig = require('mason-lspconfig')
 
-    -- Filetype detection fix for Terraform/HCL
-    vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-      pattern = {"*.tf", "*.tfvars", "*.hcl"},
-      callback = function()
-        vim.bo.filetype = "terraform"
-      end,
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				'terraformls',
+				'tflint',
+				'ts_ls',
+				'html',
+				'cssls',
+				'lua_ls',
+				'rust_analyzer'
+			},
+		})
 
-    -- Format on save (Added *.rs)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = {
-        "*.tf", "*.tfvars", "*.js", "*.ts", 
-        "*.html", "*.css", "*.lua", "*.rs"
-      },
-      callback = function()
-        vim.lsp.buf.format()
-      end,
-    })
-  end,
+		-- The New 0.11+ Handler
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				if server_name == "lua_ls" then
+					vim.lsp.config("lua_ls", {
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								workspace = { checkThirdParty = false },
+							},
+						},
+					})
+				end
+
+				-- Enable the server
+				vim.lsp.enable(server_name)
+			end,
+		})
+
+		-- Filetype detection fix for Terraform/HCL
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = { "*.tf", "*.tfvars", "*.hcl" },
+			callback = function()
+				vim.bo.filetype = "terraform"
+			end,
+		})
+
+		-- Format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = {
+				"*.tf", "*.tfvars", "*.js", "*.ts",
+				"*.html", "*.css", "*.lua", "*.rs"
+			},
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
+	end,
 }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,18 +1,50 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup({
-	  sync_root_with_cwd = true,     -- Changes the tree when you :cd
-  	  respect_buf_cwd = true,        -- Respects the folder of the file you are in
-  	  update_focused_file = {
-    	    enable = true,
-    	    update_root = true,          -- This is the "magic" setting
-  	  },
-	})
-    end,
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons", "akinsho/toggleterm.nvim" },
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		-- Função para abrir o ToggleTerm no diretório do nó selecionado
+		local function open_terminal_at_node()
+			local api = require("nvim-tree.api")
+			local node = api.tree.get_node_under_cursor()
+
+			-- Pega o caminho do nó ou o diretório atual como fallback
+			local path = node.absolute_path or vim.uv.cwd()
+
+			-- Se for um arquivo, pegamos o diretório pai (folder) dele
+			if node.type ~= "directory" then
+				path = vim.fn.fnamemodify(path, ":h")
+			end
+
+			-- Abre o terminal enviando o comando 'cd' para a pasta selecionada
+			-- O número '1' é o ID do terminal
+			require("toggleterm").exec("cd " .. path .. " && clear", 1)
+			require("toggleterm").toggle(1)
+		end
+
+		require("nvim-tree").setup({
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+			-- Configuração dos mapeamentos customizados
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- Aplica os mapeamentos padrão do Nvim-Tree primeiro
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- Adiciona o mapeamento para abrir o terminal com a tecla 'T'
+				vim.keymap.set("n", "T", open_terminal_at_node, opts("Open Terminal at Path"))
+			end,
+		})
+	end,
 }
-
-
