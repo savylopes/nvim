2c3366bd928127cf1168f8809dea0a79832c2906
Boris Lopes <boris@yolobank.com>
2026-03-04 13:53:00 -0100

    WIP

--- lua/plugins/mason-lsconfig.lua
+++ lua/plugins/mason-lsconfig.lua
@@ -6,38 +6,35 @@ return {
 		{ 'j-hui/fidget.nvim',                opts = {} },
 	},
 	config = function()
-		-- 1. Global Diagnostic Settings (This makes errors/warnings visible)
+		-- 1. Global Diagnostic Settings
 		vim.diagnostic.config({
 			virtual_text = {
-				prefix = '●', -- Displays the message at the end of the line
+				prefix = '●',
 				spacing = 4,
 			},
-			signs = true, -- Show icons in the gutter
-			underline = true, -- Underline error text
+			signs = true,
+			underline = true,
 			update_in_insert = false,
 			severity_sort = true,
 			float = {
 				border = 'rounded',
-				source = 'always', -- Shows which LSP provided the error
+				source = 'always',
 			},
 		})
-
-		-- 2. Set Gutter Icons (Optional but highly recommended)
+		-- 2. Gutter Icons
 		local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "»" }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 		end
-
-		-- 3. Diagnostic Keymaps (To navigate and read errors)
+		-- 3. Diagnostic Keymaps
 		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
 		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
 		vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show error message' })
 		vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic list' })
 
-		local mason_lspconfig = require('mason-lspconfig')
-
-		mason_lspconfig.setup({
+		-- 4. Mason: just ensure servers are installed
+		require('mason-lspconfig').setup({
 			ensure_installed = {
 				'terraformls',
 				'tflint',
@@ -45,34 +42,73 @@ return {
 				'html',
 				'cssls',
 				'lua_ls',
-				'rust_analyzer'
+				'rust_analyzer',
 			},
 		})
 
-		-- The New 0.11+ Handler
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				if server_name == "lua_ls" then
-					vim.lsp.config("lua_ls", {
-						settings = {
-							Lua = {
-								diagnostics = { globals = { "vim" } },
-								workspace = { checkThirdParty = false },
-							},
-						},
-					})
-				end
-
-				-- Enable the server
-				vim.lsp.enable(server_name)
-			end,
+		-- 5. Configure each LSP server directly via vim.lsp.config (nvim 0.11+)
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = { globals = { "vim" } },
+					workspace = { checkThirdParty = false },
+				},
+			},
 		})
 
-		-- Filetype detection fix for Terraform/HCL
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+					},
+					checkOnSave = {
+						command = "clippy",
+						extraArgs = { "--no-deps" },
+					},
+					procMacro = {
+						enable = true,
+					},
+					inlayHints = {
+						bindingModeHints = { enable = true },
+						chainingHints = { enable = true },
+						closingBraceHints = { enable = true },
+						parameterHints = { enable = true },
+						typeHints = { enable = true },
+					},
+					diagnostics = {
+						enable = true,
+						experimental = { enable = true },
+					},
+				},
+			},
+		})
+
+		-- 6. Enable all servers (picks up the configs above)
+		vim.lsp.enable({
+			'terraformls',
+			'tflint',
+			'ts_ls',
+			'html',
+			'cssls',
+			'lua_ls',
+			'rust_analyzer',
+		})
+
+		-- Filetype detection for Terraform/HCL
 		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 			pattern = { "*.tf", "*.tfvars", "*.hcl" },
 			callback = function()
 				vim.bo.filetype = "terraform"
+			end,
+		})
+
+		-- Filetype detection for Rust
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = { "*.rs" },
+			callback = function()
+				vim.bo.filetype = "rust"
 			end,
 		})
 
@@ -83,7 +119,7 @@ return {
 				"*.html", "*.css", "*.lua", "*.rs"
 			},
 			callback = function()
-				vim.lsp.buf.format()
+				vim.lsp.buf.format({ async = false })
 			end,
 		})
 	end,
